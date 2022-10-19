fc04a3967059b10cc34281c5d28c85dd846f32c5
SerikaShimabukuro <115777771+SerikaShimabukuro@users.noreply.github.com>
2022-10-19 16:43:04 +0900

    画面で入力された値を取得

--- create.php
+++ create.php
@@ -1,1 +1,17 @@
-<?php?>
+<?php
+
+// 処理の流れ
+// １．画面で入力された処理を取得する
+// ２．PHPからMySQLへ接続
+// ３．SQL文を作成して画面で入力された値をrecordsテーブルに追加
+// ４．index.phpに画面遷移する
+
+// 1. 画面で入力された値を取得
+$date = $_POST['date'];
+$title = $_POST['title'];
+$amount = $_POST['amount'];
+$type = $_POST['type'];
+
+
+
+?>
