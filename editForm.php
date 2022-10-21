b96505f67f0abfdd06a402d8f0df23619fdfcd70
SerikaShimabukuro <115777771+SerikaShimabukuro@users.noreply.github.com>
2022-10-21 14:52:57 +0900

    update実装

--- editForm.php
+++ editForm.php
@@ -58,6 +58,7 @@
     </header>
 
     <form class="m-5" action="./update.php" method="POST">
+      <input type="hidden" name="id" value="<?php echo $id; ?>">
       <p class="alert alert-success" role="alert">編集フォーム</p>
       <div class="form-group">
         <label for="date">日付</label>
