c868965741e43782ee9aea0308f98bfd927e6b90
SerikaShimabukuro <115777771+SerikaShimabukuro@users.noreply.github.com>
2022-10-21 16:22:35 +0900

    DELETE実装

---
+++ delete.php
@@ -0,0 +1,21 @@
+<?php
+
+    include_once('./dbconnect.php');
+
+    $id = $_GET['id'];
+    // var_dump($id);
+
+    // SQL文を準備
+    $sql = "DELETE FROM records WHERE id = :id";
+
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':id', $id, POD::PARAM_INT);
+    $stmt->execute();
+
+    
+
+
+
+
+
+?>
